3ff385ee0db88b4b951f15e53f5823d328a99f5a
Sarvess Veeriyah <sarvess.jr@outlook.com>
2022-08-29 10:51:59 +0800

    main

--- components/DonateButton.js
+++ components/DonateButton.js
@@ -1,7 +1,7 @@
 function DonateButton() {
   return (
     <form action="https://www.buymeacoffee.com/sarvessveeri" target="_blank" className="mb-2">
-      <button type="submit" className="bg-yellow-600 hover:bg-yellow-600 text-gray-900 font-bold uppercase p-1 rounded-lg px-4">Buy Me A Coffee</button>
+      <button type="submit" className="bg-yellow-500 hover:bg-yellow-500 text-gray-900 font-bold uppercase p-1 rounded-lg px-4">Buy Me A Coffee</button>
     </form>
   )
 }
