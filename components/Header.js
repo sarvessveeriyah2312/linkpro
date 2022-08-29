ed20558d4652e603f023a5379da3674c8f2fe509
Sarvess Veeriyah <sarvess.jr@outlook.com>
2022-08-29 11:24:03 +0800

    main

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ function Header() {
   return (
     <header className="container flex text-center mx-auto py-10">
       <div className="md:w-4/6 mx-auto">
-        <img src="/profile-pic.jpg" alt="Profile Picture" className="h-24 rounded-full mx-auto mb-6" />
+        <img src="profile-pic.jpg" alt="Profile Picture" className="h-24 rounded-full mx-auto mb-6" />
         <h1 className="font-bold">Sarvess Veeriyah</h1>
         <p>Software Engineering Student</p>
       </div>
